8728e7dd2618eaafb1bdcc0ffc258956a07085a3
JakeHresh <jakeyhresh123@gmail.com>
2021-11-15 21:32:21 -0700

    Added tic-tac-toe logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use bevy::{prelude::*, render::camera::Camera};
 use bevy_asset_loader::{AssetCollection, AssetLoader};
+use rand::prelude::*;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -24,6 +25,7 @@ pub fn run() {
     app.init_resource::<CursorWorldPos>()
         .add_state(GameState::Load)
         .add_event::<ClickEvent>()
+        .add_event::<AIEvent>()
         .add_system_set(SystemSet::on_enter(GameState::Game).with_system(init.system()))
         .add_system_set(
             SystemSet::on_update(GameState::Game)
@@ -44,6 +46,12 @@ pub fn run() {
                         .system()
                         .label("consume_click_event")
                         .after("detect_click"),
+                )
+                .with_system(
+                    ai_click
+                        .system()
+                        .label("ai_click")
+                        .after("consume_click_event"),
                 ),
         )
         .run();
@@ -75,71 +83,118 @@ fn init(
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
     commands.spawn_bundle(SpriteBundle {
-        material: materials.add(textures.x.clone().into()),
+        material: materials.add(textures.board.clone().into()),
         ..SpriteBundle::default()
     });
+    let text = materials.add(textures.unclicked.clone().into());
+    let text1 = materials.add(textures.x.clone().into());
+    let text2 = materials.add(textures.o.clone().into());
 
+    commands.insert_resource(Materials {
+        matUnclicked: text.clone(),
+        matX: text1.clone(),
+        matO: text2.clone(),
+    });
     // TEMP for demonstration purposes
-    /*commands
-        .spawn()
-        .insert(Clickable {
-            half_extents: Vec2::splat(100.),
-        })
-        .insert(Transform::default())
-        .insert(Sprite::textures.x);*/
-    commands
+    let middle_middle = commands
         .spawn_bundle(SpriteBundle {
-            material: materials.add(textures.x.clone().into()),
+            material: text.clone(),
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
             ..SpriteBundle::default()
-        });
-    commands
-        .spawn()
+        })
         .insert(Clickable {
             half_extents: Vec2::splat(100.),
         })
-        .insert(Transform::default()); // Use Transform::from_xyz(float1, float2, float3)DEFINE POSITIONS FOR THESE CLICKABLES. HAVE ANOTHER COMPONENT ATTACHED TO CLICKABLES SO IT"S KNOWN WHAT'S CLICKED AND SYSTEMS CAN BE BYPASSED. LOOK FOR WHAT IMAGE ALREADY EXISTS ON COMPONENT. THIS IS THE EASIEST IMPLEMENTAION.
-    commands
-        .spawn()
+        .id();
+    let right_middle = commands
+        .spawn_bundle(SpriteBundle {
+            material: text.clone(),
+            transform: Transform::from_xyz(200.0, 0.0, 0.0),
+            ..SpriteBundle::default()
+        })
         .insert(Clickable {
             half_extents: Vec2::splat(100.),
         })
-        .insert(Transform::default());
-    commands
-        .spawn()
+        .id();
+    let left_middle = commands
+        .spawn_bundle(SpriteBundle {
+            material: text.clone(),
+            transform: Transform::from_xyz(-200.0, 0.0, 0.0),
+            ..SpriteBundle::default()
+        })
         .insert(Clickable {
             half_extents: Vec2::splat(100.),
         })
-        .insert(Transform::default());
-    commands
-        .spawn()
+        .id();
+    let left_bottom = commands
+        .spawn_bundle(SpriteBundle {
+            material: text.clone(),
+            transform: Transform::from_xyz(-200.0, -200.0, 0.0),
+            ..SpriteBundle::default()
+        })
         .insert(Clickable {
             half_extents: Vec2::splat(100.),
         })
-        .insert(Transform::default());
-    commands
-        .spawn()
+        .id();
+    let right_bottom = commands
+        .spawn_bundle(SpriteBundle {
+            material: text.clone(),
+            transform: Transform::from_xyz(200.0, -200.0, 0.0),
+            ..SpriteBundle::default()
+        })
         .insert(Clickable {
             half_extents: Vec2::splat(100.),
         })
-        .insert(Transform::default());
-    commands
-        .spawn()
+        .id();
+    let middle_bottom = commands
+        .spawn_bundle(SpriteBundle {
+            material: text.clone(),
+            transform: Transform::from_xyz(0.0, -200.0, 0.0),
+            ..SpriteBundle::default()
+        })
         .insert(Clickable {
             half_extents: Vec2::splat(100.),
         })
-        .insert(Transform::default());
-    commands
-        .spawn()
+        .id();
+    let middle_top = commands
+        .spawn_bundle(SpriteBundle {
+            material: text.clone(),
+            transform: Transform::from_xyz(0.0, 200.0, 0.0),
+            ..SpriteBundle::default()
+        })
         .insert(Clickable {
             half_extents: Vec2::splat(100.),
         })
-        .insert(Transform::default());
-    commands
-        .spawn()
+        .id();
+    let right_top = commands
+        .spawn_bundle(SpriteBundle {
+            material: text.clone(),
+            transform: Transform::from_xyz(200.0, 200.0, 0.0),
+            ..SpriteBundle::default()
+        })
         .insert(Clickable {
             half_extents: Vec2::splat(100.),
         })
-        .insert(Transform::default());
+        .id();
+    let left_top = commands
+        .spawn_bundle(SpriteBundle {
+            material: text.clone(),
+            transform: Transform::from_xyz(-200.0, 200.0, 0.0),
+            ..SpriteBundle::default()
+        })
+        .insert(Clickable {
+            half_extents: Vec2::splat(100.),
+        })
+        .id();
+
+    let clickableEntities = ClickableEntities {
+        sections: [
+            [left_top, middle_top, right_top],
+            [left_middle, middle_middle, right_middle],
+            [left_bottom, middle_bottom, right_bottom],
+        ],
+    };
+    commands.insert_resource(clickableEntities);
 }
 
 #[derive(Default)]
@@ -194,9 +249,47 @@ fn detect_click(
     }
 }
 
+struct Materials {
+    matUnclicked: Handle<ColorMaterial>,
+    matX: Handle<ColorMaterial>,
+    matO: Handle<ColorMaterial>,
+}
+
+struct AIEvent;
+
 // TEMP for demonstration purposes
-fn consume_click_event(mut click_events: EventReader<ClickEvent>) {
-    click_events.iter().for_each(|_| {
-        println!("Clicked!");
+fn consume_click_event(
+    mut ai_event: EventWriter<AIEvent>,
+    mut click_events: EventReader<ClickEvent>,
+    mut material: Query<&mut Handle<ColorMaterial>>,
+    matCompare: Res<Materials>,
+) {
+    click_events.iter().for_each(|click_event| {
+        if *material.get_mut(click_event.0).unwrap() == matCompare.matUnclicked {
+            *material.get_mut(click_event.0).unwrap() = matCompare.matX.clone();
+            ai_event.send(AIEvent);
+        }
+    });
+}
+
+struct ClickableEntities {
+    //[type;size]
+    sections: [[Entity; 3]; 3],
+}
+
+fn ai_click(
+    mut ai_events: EventReader<AIEvent>,
+    clickable_entities: Res<ClickableEntities>,
+    mut material: Query<&mut Handle<ColorMaterial>>,
+    matCompare: Res<Materials>,
+) {
+    ai_events.iter().for_each(|ai_event| loop {
+        let x = rand::thread_rng().gen_range(0..3);
+        let y = rand::thread_rng().gen_range(0..3);
+        if *material.get_mut(clickable_entities.sections[y][x]).unwrap() == matCompare.matUnclicked
+        {
+            *material.get_mut(clickable_entities.sections[y][x]).unwrap() = matCompare.matO.clone();
+            break;
+        }
     });
 }
